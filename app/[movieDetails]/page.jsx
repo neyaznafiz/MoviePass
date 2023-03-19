2bcbbaf9fad84446f3db0bb747a6346ceb3aecba
neyaznafiz <neyaznafiz@outlook.com>
2023-03-20 00:04:31 +0600

    some details and function added

--- app/[movieDetails]/page.jsx
+++ app/[movieDetails]/page.jsx
@@ -1,15 +1,31 @@
 import Image from "next/image";
 import React from "react";
 
+
+// export const revalidate = 0
+
+export const generateStaticParams = async()=>{
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+  
+  return res.results.map((movie)=> {
+    movie: toString(movie.id)
+  })
+}
+
 const MovieDetails = async ({ params }) => {
   const { movieDetails } = params;
 
   const imagePath = "https://image.tmdb.org/t/p/original";
 
   const data = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieDetails}?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/movie/${movieDetails}?api_key=${process.env.API_KEY}`, {next: {revalidate: 0}}
   );
+  // const data = await prisma.posts.findMany({next: {revalidate: 0}})
   const res = await data.json();
+  console.log(res)
 
   return (
     <div className="w-max mx-auto my-14 flex gap-x-10">
@@ -24,6 +40,7 @@ const MovieDetails = async ({ params }) => {
 
       <div>
         <h1 className="text-4xl font-bold">{res.title}</h1>
+        <p>{res.tagline}</p>
         <h2 className="text-sm text-gray-400">
           Relase Date: {res.release_date}
         </h2>
@@ -31,6 +48,9 @@ const MovieDetails = async ({ params }) => {
         <h2 className="bg-green-600 my-2 px-4 rounded-tr-xl rounded-bl-xl inline-block">
           {res.status}
         </h2>
+
+        <h3>Vote: {res.vote_average}</h3>
+        <h3>Revenue: {res.revenue}</h3>
 
         <p className="w-[500px] my-2">{res.overview}</p>
       </div>
